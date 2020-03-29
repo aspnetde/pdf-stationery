9a348e7d25b931db7f0f56ba34591c0711e49782
Thomas Bandt <bandt@69grad.de>
2020-03-29 15:08:18 +0200

    Nicer View Formatting

--- src/PdfStationery/Stationery.fs
+++ src/PdfStationery/Stationery.fs
@@ -30,6 +30,7 @@ module Stationery =
         | NoOp
 
     type Program(parent: HostWindow) =
+
         let init() =
             let config = Config.get
             { ReplaceSource = config.ReplaceSource
@@ -44,13 +45,12 @@ module Stationery =
             NoOp
 
         let canPrint state =
-            not (String.IsNullOrWhiteSpace(state.TemplatePath)) &&
-            not (String.IsNullOrWhiteSpace(state.SourcePath))
+            not (String.IsNullOrWhiteSpace(state.TemplatePath)) && not (String.IsNullOrWhiteSpace(state.SourcePath))
 
         let print state =
             if state.ReplaceSource then
                 Printer.replacePdf state.SourcePath state.TemplatePath
-                ShowSuccessMessage (SuccessMessageWithReplacement |> translate)
+                ShowSuccessMessage(SuccessMessageWithReplacement |> translate)
             else
                 let tmpPath = Printer.newPdf state.SourcePath state.TemplatePath
                 CmdSelectStorageLocationForNewPdf tmpPath
@@ -71,90 +71,93 @@ module Stationery =
             | ToggleReplaceSource -> { state with ReplaceSource = not state.ReplaceSource }, Cmd.ofMsg CmdUpdateConfig
             | ChangeTemplatePath path -> { state with TemplatePath = path }, Cmd.ofMsg CmdUpdateConfig
             | ChangeSourcePath path -> { state with SourcePath = path }, Cmd.ofMsg CmdUpdateConfig
-
             | CmdPrint -> state, Cmd.ofMsg (print state)
             | CmdUpdateConfig -> state, Cmd.ofMsg (updateConfig state)
-
             | ShowSuccessMessage message -> state, Cmd.OfAsync.either showSuccess message noOp noOp
             | ShowErrorMessage ex -> state, Cmd.OfAsync.either showError ex noOp noOp
-
             | CmdOpenFileDialogForSource ->
-                state, Cmd.OfAsync.either openFileDialog (state.TemplatePath, parent) ChangeTemplatePath ShowErrorMessage
+                state,
+                Cmd.OfAsync.either openFileDialog (state.TemplatePath, parent) ChangeTemplatePath ShowErrorMessage
             | CmdOpenFileDialogForTemplate ->
-                state, Cmd.OfAsync.either openFileDialog (state.SourcePath, parent) ChangeSourcePath ShowErrorMessage
+                state,
+                Cmd.OfAsync.either openFileDialog (state.SourcePath, parent) ChangeSourcePath ShowErrorMessage
             | CmdSelectStorageLocationForNewPdf path ->
-                let args = { SourcePath = state.SourcePath; TmpPath = path }
+                let args =
+                    { SourcePath = state.SourcePath
+                      TmpPath = path }
                 state, Cmd.OfAsync.either openSaveDialog (args, parent) savePdf ShowErrorMessage
-
             | NoOp -> state, Cmd.none
 
         let view state dispatch =
-            DockPanel.create
-                [ DockPanel.children
-                    [ StackPanel.create
-                        [ StackPanel.dock Dock.Bottom
-                          StackPanel.margin 15.0
-                          StackPanel.spacing 5.0
-                          StackPanel.children
-                              [ TextBlock.create [
-                                  TextBlock.text (FormTemplatePdfLabel |> translate)
-                                  TextBlock.width 390.0
-                                ]
-                                StackPanel.create
-                                    [ StackPanel.orientation Orientation.Horizontal
-                                      StackPanel.width 390.0
-                                      StackPanel.spacing 5.0
-                                      StackPanel.children
-                                          [ TextBox.create
-                                              [ TextBox.width 285.0
-                                                TextBox.text state.TemplatePath
-                                                TextBox.isEnabled false ]
-                                            Button.create
-                                                [ Button.content (FormSelectButton |> translate)
-                                                  Button.verticalAlignment VerticalAlignment.Center
-                                                  Button.width 100.0
-                                                  Button.onClick (fun _ -> dispatch CmdOpenFileDialogForSource) ] ] ]
-                                Separator.create [ Separator.height 5.0 ]
+            DockPanel.create [
+                DockPanel.children [
+                    StackPanel.create [
+                        StackPanel.dock Dock.Bottom
+                        StackPanel.margin 15.0
+                        StackPanel.spacing 5.0
+                        StackPanel.children [
+                            TextBlock.create [
+                                TextBlock.text (FormTemplatePdfLabel |> translate)
+                                TextBlock.width 390.0 ]
+                            
+                            StackPanel.create [
+                                StackPanel.orientation Orientation.Horizontal
+                                StackPanel.width 390.0
+                                StackPanel.spacing 5.0
+                                StackPanel.children [
+                                    TextBox.create [
+                                        TextBox.width 285.0
+                                        TextBox.text state.TemplatePath
+                                        TextBox.isEnabled false ]
+                                    Button.create [
+                                        Button.content (FormSelectButton |> translate)
+                                        Button.verticalAlignment VerticalAlignment.Center
+                                        Button.width 100.0
+                                        Button.onClick (fun _ -> dispatch CmdOpenFileDialogForSource) ] ] ]
+                                
+                            Separator.create [ Separator.height 5.0 ]
 
-                                TextBlock.create [
-                                    TextBlock.text (FormOriginalPdfLabel |> translate)
-                                    TextBlock.width 390.0
-                                ]
-                                StackPanel.create
-                                    [ StackPanel.orientation Orientation.Horizontal
-                                      StackPanel.width 390.0
-                                      StackPanel.spacing 5.0
-                                      StackPanel.children
-                                          [ TextBox.create
-                                              [ TextBox.width 285.0
-                                                TextBox.text state.SourcePath
-                                                TextBox.isEnabled false ]
-                                            Button.create
-                                                [ Button.content (FormSelectButton |> translate)
-                                                  Button.width 100.0
-                                                  Button.verticalAlignment VerticalAlignment.Center
-                                                  Button.onClick (fun _ -> dispatch CmdOpenFileDialogForTemplate) ] ] ]
-                                Separator.create [ Separator.height 5.0 ]
+                            TextBlock.create [
+                                TextBlock.text (FormOriginalPdfLabel |> translate)
+                                TextBlock.width 390.0 ]
+                            
+                            StackPanel.create [
+                                StackPanel.orientation Orientation.Horizontal
+                                StackPanel.width 390.0
+                                StackPanel.spacing 5.0
+                                StackPanel.children [
+                                    TextBox.create [
+                                        TextBox.width 285.0
+                                        TextBox.text state.SourcePath
+                                        TextBox.isEnabled false ]
+                                    Button.create [
+                                        Button.content (FormSelectButton |> translate)
+                                        Button.width 100.0
+                                        Button.verticalAlignment VerticalAlignment.Center
+                                        Button.onClick (fun _ -> dispatch CmdOpenFileDialogForTemplate) ] ] ]
+                            
+                            Separator.create [ Separator.height 5.0 ]
 
-                                StackPanel.create
-                                    [ StackPanel.orientation Orientation.Horizontal
-                                      StackPanel.width 390.0
-                                      StackPanel.spacing 10.0
-                                      StackPanel.children
-                                          [ CheckBox.create
-                                              [ CheckBox.isChecked state.ReplaceSource
-                                                CheckBox.onTapped (fun _ -> dispatch ToggleReplaceSource) ]
-                                            TextBlock.create
-                                                [ TextBlock.text (FormReplacementLabel |> translate)
-                                                  TextBlock.onTapped (fun _ -> dispatch ToggleReplaceSource)
-                                                  TextBlock.verticalAlignment VerticalAlignment.Center ] ] ]
-                                Separator.create [ Separator.height 5.0 ]
+                            StackPanel.create [
+                                StackPanel.orientation Orientation.Horizontal
+                                StackPanel.width 390.0
+                                StackPanel.spacing 10.0
+                                StackPanel.children [
+                                    CheckBox.create [
+                                        CheckBox.isChecked state.ReplaceSource
+                                        CheckBox.onTapped (fun _ -> dispatch ToggleReplaceSource) ]
+                                    TextBlock.create [
+                                        TextBlock.text (FormReplacementLabel |> translate)
+                                        TextBlock.onTapped (fun _ -> dispatch ToggleReplaceSource)
+                                        TextBlock.verticalAlignment VerticalAlignment.Center ] ] ]
+                            
+                            Separator.create [ Separator.height 5.0 ]
 
-                                Button.create
-                                    [ Button.content (FormPrintButton |> translate)
-                                      Button.width 390.0
-                                      Button.onClick (fun _ -> dispatch CmdPrint)
-                                      Button.isEnabled (state |> canPrint) ] ] ] ] ]
+                            Button.create [
+                                Button.content (FormPrintButton |> translate)
+                                Button.width 390.0
+                                Button.onClick (fun _ -> dispatch CmdPrint)
+                                Button.isEnabled (state |> canPrint) ] ] ] ] ]
 
         do
             Elmish.Program.mkProgram init update view
